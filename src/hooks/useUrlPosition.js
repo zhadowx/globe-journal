23b9709ee8184afd316d8a250ada37060bb0d9ce
Nelson Lora <nlorax@gmail.com>
2024-02-28 10:42:14 -0400

    Remove useUrlPosition hook

--- src/hooks/useUrlPosition.js
+++
@@ -1,9 +0,0 @@
-import { useSearchParams } from "react-router-dom";
-
-export function useURLPosition() {
-  const [searchParams] = useSearchParams();
-
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-  return [lat, lng];
-}
