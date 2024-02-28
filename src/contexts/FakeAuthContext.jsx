166ee72eb50e1f4996a0ac4dccc34dac9de49eff
Nelson Lora <nlorax@gmail.com>
2024-02-28 16:56:35 -0400

    Add fake authentication and route protection

---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,67 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload,
+      };
+    case "logout":
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      throw new Error(`Unhandled action type`);
+  }
+}
+
+const FAKE_USER = {
+  name: "Nelson",
+  email: "nelson@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    }
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+}
+
+export { AuthProvider, useAuth };
